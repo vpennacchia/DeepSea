3f588996ce2811062416d9dc620fb3fc9ec3ccdf
Vitto00 <penna.vitto@gmail.com>
2022-04-07 17:32:08 +0200

    Added mex size = 1 case

--- src/Impl/Dec.java
+++ src/Impl/Dec.java
@@ -3,6 +3,8 @@ package Impl;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import static java.lang.Math.pow;
+
 public class Dec {
     public static String decoding(Mex mex) {
         String m = mex.getMex();
@@ -11,42 +13,50 @@ public class Dec {
         String dec_mex = "";
         int i, j;
 
-        j = (dec.length) / 2;
-        for (i = 0; i <= ((dec.length) / 2) - 1 && j <= dec.length; ++i, ++j) {
-            char s = dec[j];
-            dec[j] = dec[i];
-            dec[i] = s;
+        if(m.length() == 1){
+            if(SpecialChar.contains(0)){
+                dec[0] = (char) ((int) (dec[0]) - 50);
+            }
+            else{
+                dec[0] =  (char) (((int) dec[0] - ((1 * 3) + 5) - dec.length - 1));
+            }
         }
-        if (dec.length % 2 == 0) {
+        else {
+
             j = (dec.length) / 2;
-            char el0 = dec[0];
-            dec[0] = dec[j - 1];
-            dec[j - 1] = el0;
-            char el1 = dec[j];
-            dec[j] = dec[dec.length - 1];
-            dec[dec.length - 1] = el1;
-        }
+            for (i = 0; i <= ((dec.length) / 2) - 1 && j <= dec.length; ++i, ++j) {
+                char s = dec[j];
+                dec[j] = dec[i];
+                dec[i] = s;
+            }
+            if (dec.length % 2 == 0) {
+                j = (dec.length) / 2;
+                char el0 = dec[0];
+                dec[0] = dec[j - 1];
+                dec[j - 1] = el0;
+                char el1 = dec[j];
+                dec[j] = dec[dec.length - 1];
+                dec[dec.length - 1] = el1;
+            }
 
-        for (i = 0; i <= dec.length - 1; ++i) {
-            if (dec[i] == '%' || dec[i] == '$' || dec[i] == '#' || dec[i] == '@') {
-                dec[i] = ' ';
-             }
-
-            if (dec[i] != ' ') {
-                if(i % 2 == 0) {
-                    if(SpecialChar.contains(i)){
-                        dec[i] = (char) ( (int) (dec[i]) - 50);
-                    }
-                    else {
-                        dec[i] = (char) (((int) dec[i] - (i * 3)) - 5 - dec.length - i);
-                    }
+            for (i = 0; i <= dec.length - 1; ++i) {
+                if (dec[i] == '%' || dec[i] == '$' || dec[i] == '#' || dec[i] == '@') {
+                    dec[i] = ' ';
                 }
-                else {
-                    if(SpecialChar.contains(i)){
-                        dec[i] = (char) ( (int) (dec[i]) - 40);
-                    }
-                    else {
-                        dec[i] = (char) (((int) dec[i] - (((i * 6) + 13) + dec.length + (i + 4) * (dec.length / 2))));
+
+                if (dec[i] != ' ') {
+                    if (i % 2 == 0) {
+                        if (SpecialChar.contains(i)) {
+                            dec[i] = (char) ((int) (dec[i]) - 50);
+                        } else {
+                            dec[i] = (char) (((int) dec[i] - (i * 3)) - 5 - dec.length - i);
+                        }
+                    } else {
+                        if (SpecialChar.contains(i)) {
+                            dec[i] = (char) ((int) (dec[i]) - 40);
+                        } else {
+                            dec[i] = (char) (((int) dec[i] - (((i * 6) + 13) + dec.length + (i + 4) * (dec.length / 2))));
+                        }
                     }
                 }
             }
@@ -55,9 +65,9 @@ public class Dec {
         for (j = 0; j <= dec.length - 1; ++j) {
                 dec_mex = dec_mex + dec[j];
         }
-
         return dec_mex;
     }
-}
+  }
+
 
 
--- src/Impl/Enc.java
+++ src/Impl/Enc.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
 
+import static java.lang.Math.pow;
+
 /*VP01*/
 public class Enc {
     public static Mex encoding(String mex) {
@@ -14,63 +16,75 @@ public class Enc {
         int i = 0, space=0, j=0;
         ArrayList<Integer> SpecialChar = new ArrayList<>();
 
-        for(i=0; i<= encode.length - 1; ++i) {
-            int utf = (char) (((int) encode[i] + ((i * 3) + 5) + encode.length + i));
-            int utf2 = (char) (((int) encode[i] + ((i * 6) + 13) + encode.length + (i + 4) * (encode.length / 2)));
-            if (encode[i] != ' ') {
-                if (i % 2 == 0) {
-                    if (utf == 0 || (utf >= 127 && utf <= 159) || utf > 1824) {
-                        encode[i] = (char) ((int) (encode[i]) + 50);
-                        SpecialChar.add(i);
+        if(mex.length() == 1){
+            int utf =  (char) (((int) encode[0] + ((1 * 3) + 5) + encode.length + 1));
+            if (utf == 0 || (utf >= 127 && utf <= 159) || utf > 1824) {
+                encode[0] = (char) ((int) (encode[0]) + 50);
+                SpecialChar.add(0);
+            }
+            else{
+                encode[0] = (char) (((int) encode[0] + ((1 * 3) + 5) + encode.length + 1));
+            }
+        }
+        else {
+
+            for (i = 0; i <= encode.length - 1; ++i) {
+                int utf = (char) (((int) encode[i] + ((i * 3) + 5) + encode.length + i));
+                int utf2 = (char) (((int) encode[i] + ((i * 6) + 13) + encode.length + (i + 4) * (encode.length / 2)));
+                if (encode[i] != ' ') {
+                    if (i % 2 == 0) {
+                        if (utf == 0 || (utf >= 127 && utf <= 159) || utf > 1824) {
+                            encode[i] = (char) ((int) (encode[i]) + 50);
+                            SpecialChar.add(i);
+                        } else {
+                            encode[i] = (char) (((int) encode[i] + ((i * 3) + 5) + encode.length + i));
+                        }
                     } else {
-                        encode[i] = (char) (((int) encode[i] + ((i * 3) + 5) + encode.length + i));
+                        if (utf2 == 0 || (utf2 >= 127 && utf2 <= 159) || utf2 > 1824) {
+                            encode[i] = (char) ((int) (encode[i]) + 40);
+                            SpecialChar.add(i);
+                        } else {
+                            encode[i] = (char) (((int) encode[i] + ((i * 6) + 13) + encode.length + (i + 4) * (encode.length / 2)));
+                        }
                     }
-                } else {
-                    if (utf2 == 0 || (utf2 >= 127 && utf2 <= 159) || utf2 > 1824) {
-                        encode[i] = (char) ((int) (encode[i]) + 40);
-                        SpecialChar.add(i);
-                    } else {
-                        encode[i] = (char) (((int) encode[i] + ((i * 6) + 13) + encode.length + (i + 4) * (encode.length / 2)));
+                }
+
+                if (encode[i] == ' ') {
+                    if (space > 3) {
+                        space = 0;
                     }
+                    if (space == 0) {
+                        encode[i] = '%';
+                    }
+                    if (space == 1) {
+                        encode[i] = '$';
+                    }
+                    if (space == 2) {
+                        encode[i] = '@';
+                    }
+                    if (space == 3) {
+                        encode[i] = '#';
+                    }
+                    ++space;
                 }
             }
 
-           if (encode[i] == ' ') {
-               if (space > 3) {
-                   space = 0;
-               }
-               if (space == 0) {
-                   encode[i] = '%';
-               }
-               if (space == 1) {
-                   encode[i] = '$';
-               }
-               if (space == 2) {
-                   encode[i] = '@';
-               }
-               if (space == 3) {
-                   encode[i] = '#';
-               }
-               ++space;
-           }
-       }
-
-       j = (encode.length) / 2;
-       for (i = 0; i <= ((encode.length) / 2) - 1 && j <= encode.length; ++i, ++j) {
-           char s = encode[i];
-           encode[i] = encode[j];
-           encode[j] = s;
-       }
-       if (encode.length % 2 == 0) {
-           j = (encode.length) / 2;
-           char el0 = encode[0];
-           encode[0] = encode[j - 1];
-           encode[j - 1] = el0;
-           char el1 = encode[j];
-           encode[j] = encode[encode.length - 1];
-           encode[encode.length - 1] = el1;
-       }
-
+            j = (encode.length) / 2;
+            for (i = 0; i <= ((encode.length) / 2) - 1 && j <= encode.length; ++i, ++j) {
+                char s = encode[i];
+                encode[i] = encode[j];
+                encode[j] = s;
+            }
+            if (encode.length % 2 == 0) {
+                j = (encode.length) / 2;
+                char el0 = encode[0];
+                encode[0] = encode[j - 1];
+                encode[j - 1] = el0;
+                char el1 = encode[j];
+                encode[j] = encode[encode.length - 1];
+                encode[encode.length - 1] = el1;
+            }
+        }
        for (j = 0; j <= encode.length - 1; ++j) {
            enc_mex = enc_mex + encode[j];
        }
@@ -80,5 +94,4 @@ public class Enc {
 
         return m;
     }
-
 }
